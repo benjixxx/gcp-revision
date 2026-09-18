d3e107c96a46cb155c39dd9087a47d68d3064ed0
benjixxx <benjamin.laurent59@gmail.com>
2026-09-18 16:20:30 +0200

    add role gke githubsa fix

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -84,7 +84,6 @@ resource "google_service_account" "github_sa" {
   project      = var.project_id
 }
 
-# 1. Grant roles/artifactregistry.writer and roles/container.developer to this SA
 # 1. Grant roles/artifactregistry.writer and roles/container.admin to this SA
 resource "google_project_iam_member" "github_sa_artifact_writer" {
   project = var.project_id
@@ -92,10 +91,8 @@ resource "google_project_iam_member" "github_sa_artifact_writer" {
   member  = "serviceAccount:${google_service_account.github_sa.email}"
 }
 
-resource "google_project_iam_member" "github_sa_gke_developer" {
 resource "google_project_iam_member" "github_sa_gke_admin" {
   project = var.project_id
-  role    = "roles/container.developer"
   role    = "roles/container.admin"
   member  = "serviceAccount:${google_service_account.github_sa.email}"
 }
